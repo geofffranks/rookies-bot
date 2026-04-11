ea6c990fd38a4c4a722b7b0d58485a6d2dba9c57
Geoff Franks <geoff.franks@gmail.com>
2026-04-11 17:04:00 -0400

    test: add buildPenalizedDriverList unit tests

--- discord/discord_internal_test.go
+++ discord/discord_internal_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/disgoorg/snowflake/v2"
+	"github.com/geofffranks/rookies-bot/models"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -64,5 +65,49 @@ var _ = Describe("isAllowedUser", func() {
 
 	It("returns false for a non-admin user ID", func() {
 		Expect(isAllowedUser(snowflakeID(999999999))).To(BeFalse())
+	})
+})
+
+var _ = Describe("buildPenalizedDriverList", func() {
+	var driverLookup models.DriverLookup
+
+	BeforeEach(func() {
+		driverLookup = models.DriverLookup{
+			42: {FirstName: "Max", LastName: "V", CarNumber: 42, DiscordHandle: "maxv"},
+			77: {FirstName: "Valt", LastName: "B", CarNumber: 77, DiscordHandle: "valtb"},
+		}
+	})
+
+	It("returns nil slice for empty car number list", func() {
+		drivers, err := buildPenalizedDriverList(driverLookup, []int{})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(drivers).To(BeNil())
+	})
+
+	It("returns the correct drivers for known car numbers", func() {
+		drivers, err := buildPenalizedDriverList(driverLookup, []int{42, 77})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(drivers).To(HaveLen(2))
+		Expect(drivers[0].CarNumber).To(Equal(42))
+		Expect(drivers[1].CarNumber).To(Equal(77))
+	})
+
+	It("returns a single driver when one car number is given", func() {
+		drivers, err := buildPenalizedDriverList(driverLookup, []int{42})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(drivers).To(HaveLen(1))
+		Expect(drivers[0].DiscordHandle).To(Equal("maxv"))
+	})
+
+	It("returns an error when a car number is not in the lookup", func() {
+		_, err := buildPenalizedDriverList(driverLookup, []int{99})
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("99"))
+	})
+
+	It("returns an error mentioning the unknown number even when some are valid", func() {
+		_, err := buildPenalizedDriverList(driverLookup, []int{42, 99})
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("99"))
 	})
 })
